e34334b89b1f2129daede55e6f5a6298a7b215a6
Leon Weber <leon@leonweber.de>
2013-03-04 12:41:50 +0100

    Add setup.py script

---
+++ setup.py
@@ -0,0 +1,14 @@
+from distutils.core import setup
+
+desc = ( 
+    'An interface to the Pluggable Authentication Modules (PAM) library'
+    'on linux, written in pure python (using ctypes)'
+)
+
+setup(name='pam', version='0.1.4alpha',
+    description=desc,
+    py_modules=['pam'],
+    author='Leon Weber <leon@leonweber.de>, Chris AtLee <chris@atlee.ca',
+    author_email='leon@leonweber.de',
+    url='https://github.com/leonnnn/python3-pam',
+)
